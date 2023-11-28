b8b2095a566d93968090fc401d9adcb9d687c0d5
kiransai <kiransais03@gmail.com>
2023-11-28 17:34:40 +0530

    p1

---
+++ frontend_gatsby_density/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage = () => {
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
